0dbd41b5c6184c3bce2f2fd78153f01146e53058
Torsten Blindert <info@by-torsten.com>
2021-10-24 15:12:26 +0000

    Added tailwindcss & daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { render } from 'solid-js/web'
+import './style.css'
 
-const App = () => <h1>Work!</h1>
+const App = () => (
+    <div className="p-4">
+        <h1 className="btn btn-primary">Works</h1>
+        <progress class="progress progress-success" value="50" max="100"></progress> 
+    </div>
+)
 
 render(App, document.getElementById('app')!)
